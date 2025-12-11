39e95b0f852ecd4d15679458dc873cf8231a3fe0
Suraj <mynisks@gmail.com>
2025-12-11 19:47:35 +0530

    Initial commits

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <header>Dashboard Card Status</header>
+      <main>
+        Dashboard Card Status Main Body
+      </main>
+    </div>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  devIndicators: false,
+  distDir: "out",
+};
+
+export default nextConfig;
